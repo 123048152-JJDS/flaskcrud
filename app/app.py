bb85808f453ff09b212408d18f2ff2bfd4a6cec8
piranger12 <123048152@upq.edu.mx>
2026-03-23 07:11:20 +0000

    Dockerizacion: Dockerfile, docker-compose.yml, fix app.py templates y static

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 # Application initializations
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 # settings
 app.secret_key = "mysecretkey"
