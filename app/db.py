adaf6145fe1ff82331f0027d5935af17c257f8f3
PiRanger12 <123048152@upq.edu.mx>
2026-03-26 00:15:59 -0600

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,17 +1,24 @@
 from app import app
 from flask_mysqldb import MySQL
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 
-load_dotenv()  # take environment variables from .env.
+# load_dotenv()  # take environment variables from .env.
 
 # Mysql Settings
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'mysql')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
-app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))
+# app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+# app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+# app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'mysql')
+# app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+# app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))
+# app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', 'mysql')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'root')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', 'faztpassword')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'flaskcrud')
+app.config['MYSQL_PORT'] = int(os.environ.get('MYSQL_PORT', 3306))
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
 
 # MySQL Connection
 mysql = MySQL(app)
