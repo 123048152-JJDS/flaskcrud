c2b5faa7f3cf612485fdde1deb6667e1686a193f
PiRanger12 <123048152@upq.edu.mx>
2026-03-26 00:13:15 -0600

    Update db.py

--- app/db.py
+++ app/db.py
@@ -8,7 +8,7 @@ load_dotenv()  # take environment variables from .env.
 # Mysql Settings
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'mysql')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
